7bf0db52024dd833cf97af6d6f82339e4c38e9e2
serdar <steterbas@gmail.com>
2021-03-25 16:13:28 -0400

    reports

--- src/test/java/Driver/Web.java
+++ src/test/java/Driver/Web.java
@@ -1,15 +1,19 @@
 package Driver;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
 public class Web {
     private static WebDriver driver = null;
+    @BeforeMethod
     public void initDriver(){
         System.setProperty("webdriver.chrome.driver", "./driverExecFile/chromedriver");
+        driver = new ChromeDriver();
         driver.get("https://edube.org/login");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
@@ -17,6 +21,7 @@ public class Web {
     public void quitDriver(){
         driver.quit();
     }
+
     public static WebDriver getDriver(){ return driver; }
 
 }
